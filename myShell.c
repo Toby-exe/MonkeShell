6246db34e25588932766260f642885fa21a7a610
Tobias <twond799@mtroyal.ca>
2023-09-18 15:10:04 +0000

    intial commit

---
+++ myShell.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "string.h"
+
+char *getCommand(char* );
+
+int main () {
+
+    typedef enum { Running, Terminate } shellState;
+    char cmd[60];
+
+    shellState myShellState = Running;
+
+    while(myShellState == Running){
+        printf("$ ");
+        scanf("%s", cmd);
+        if(strcmp(getCommand(cmd), "exit") == 0){
+            myShellState = Terminate;
+        }
+    } 
+
+    return 0;
+}
+
+char *getCommand(char *cmd){
+    char argv[100];
+
+    printf("Command Entered: %s\n", cmd);
+
+    return cmd;
+}
