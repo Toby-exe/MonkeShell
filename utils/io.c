ef5e52bfd11b8aee378a44f361740aee01742039
Toby-exe <tobyyyw@gmail.com>
2023-10-01 02:13:38 +0000

    changed file structure. Moved utility esk programs under a utuils folder

---
+++ utils/io.c
@@ -0,0 +1,23 @@
+#include "io.h"
+
+// ==================== INPUT ==================== //
+int _scanf(const char *, ...)
+{
+    return 0;
+}
+
+int _fscanf()
+{
+    return 0;
+}
+
+// ==================== OUTPUT ==================== //
+int _printf(const char *, ...)
+{
+    return 0;
+}
+
+int _fprintf()
+{
+    return 0;
+}
---
+++ utils/io.h
@@ -0,0 +1,15 @@
+#ifndef IO_H
+#define IO_H
+
+#include "types.h"
+
+// *** custom C standard library stdio.h *** //
+//input
+int _scanf(const char *, ...);
+int _fscanf();
+
+//output
+int _printf(const char *, ...);
+int _fprintf();
+
+#endif
